32e566c978ef66a197d439fd4b4fe06d9406cd27
Zaahir Moolla <moollaza@duckduckgo.com>
2015-10-02 14:21:04 -0400

    add in wallpaper module, basic demo of functionality

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 'use strict';
-const app = require('app');
-var menubar = require('menubar')
-var mb = menubar()
-
-const BrowserWindow = require('browser-window');
 
 // report crashes to the Electron project
 require('crash-reporter').start();
@@ -11,44 +6,20 @@ require('crash-reporter').start();
 // adds debug features like hotkeys for triggering dev tools and reload
 require('electron-debug')();
 
+var wallpaper = require('wallpaper');
+var menubar = require('menubar')
+
+var mb = menubar({
+	width: 600,
+	height: 400,
+	preloadWindow: true,
+})
+
 mb.on('ready', function ready () {
   console.log('app is ready')
 
-  // prevent window being garbage collected
-  let mainWindow;
-
-  function onClosed() {
-  	// dereference the window
-  	// for multiple windows store them in an array
-  	mainWindow = null;
-  }
-
-  function createMainWindow() {
-  	const win = new BrowserWindow({
-  		width: 600,
-  		height: 400
-  	});
-
-  	win.loadUrl(`file://${__dirname}/index.html`);
-  	win.on('closed', onClosed);
-
-  	return win;
-  }
-
-  app.on('window-all-closed', () => {
-  	if (process.platform !== 'darwin') {
-  		app.quit();
-  	}
-  });
-
-  app.on('activate-with-no-open-windows', () => {
-  	if (!mainWindow) {
-  		mainWindow = createMainWindow();
-  	}
-  });
-
-  app.on('ready', () => {
-  	mainWindow = createMainWindow();
+  wallpaper.set('mountains.jpg', function (err) {
+	console.log('done');
   });
 
 })
